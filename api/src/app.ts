16543ee25d433b047c9e6ec3ebd3a4fcb10eed90
Jarosław Juszkiewicz <jj.juszkiewicz@gmail.com>
2023-01-19 20:48:17 +0100

    npm i prod

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,11 +2,14 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
+
 app.use((req, resp, next) => {
   console.log("request", req.path)
   next();
 });
+
 app.get("/api/random-numbers", async (req, res) => {
   const numbers = [];
   for (let i = 0; i < 10; ++i) {
